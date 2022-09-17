b20bcbea17fe309b6ca5225bfef2d0cbd1fca676
Kelyton Santos <kelytonlucas@gmail.com>
2022-09-17 13:25:21 -0300

    creation of new branch and first commit

---
+++ src/main/java/com/finnance/software/finnancesoftware/FinnanceSoftwareApplication.java
@@ -0,0 +1,13 @@
+package com.finnance.software.finnancesoftware;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinnanceSoftwareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinnanceSoftwareApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/finnance/software/finnancesoftware/FinnanceSoftwareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.finnance.software.finnancesoftware;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinnanceSoftwareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
